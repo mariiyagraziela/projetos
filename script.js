a3375cdfadd7db3a4d7f9b6c40413e3da0090c86
Graziela <mariagrazielasouza4@gmail.com>
2025-06-17 14:15:40 -0300

    gitando os arquivos pela primeira vez

---
+++ script.js
@@ -0,0 +1,10 @@
+let notas = document.getElementyById("notas");
+let anotacoes = document.getElementById("anotacoes");
+
+
+
+function escreverNoBloco(){
+    document.getElementById("id_mostraanotacoes_dousuario").innerHTML= anotacoes;
+}
+.forEach((p)=>{p.style.color = "red"};)
+let arrayReal = array.from(paragrafos);
