4257ca9f29abf06d3366d4e798e5a6c0dfffa8fc
Graziela <mariagrazielasouza4@gmail.com>
2026-02-24 14:41:13 -0300

    Função conteudoDentroDoCard pronta

--- projetoPortifolio2/script.js
+++ projetoPortifolio2/script.js
@@ -1,21 +1,27 @@
 let barNP = document.getElementById("barNP");
 
-
+//Função de NOVA PUBLICAÇÃO
+let telaAberta = false;
 barNP.addEventListener("click", function(){
+  if(telaAberta) return;
+  telaAberta = true;
   let modificarPrincipal =    document.getElementById("conteudoPrincipalModificar"); 
-  modificarPrincipal.innerHTML = " ";
-  let sessaocard = document.getElementById("sessao-cards").style.display="none"; 
+
+  let conteudoECards = document.getElementById("conteudoCards").style.display="none"; 
+  
  
   //input do titulo
   const input = document.createElement("input");
   input.type = "text";
   input.placeholder = "Digite o titulo da matéria"
+
   //textoarea com o conteudo
   const input2 = document.createElement("textarea");
   input2.type = "text";
   input2.setAttribute("placeholder","valor");
 
-  input.classList.add("estilo-input");// adicionado css aos input
+  //adicionado css aos input
+  input.classList.add("estilo-input");
   input2.classList.add("estilo-input2");
 
   const botoes = document.createElement("div");
@@ -26,8 +32,7 @@ barNP.addEventListener("click", function(){
   botaoSalvar.textContent = "Salvar"; 
   const botaoSair = document.createElement("button");
   botaoSair.textContent = "Sair";
-  
-         
+        
   botoes.append(botaoSalvar);
   botoes.append(botaoSair);
 
@@ -37,6 +42,7 @@ barNP.addEventListener("click", function(){
 
   //Adicionando funcionalidades para os botões de Salvar e Sair
   
+  //Uma função
   botaoSalvar.addEventListener("click", salvarpublicacao);
   function salvarpublicacao(){
       const titulo = input.value;
@@ -44,18 +50,51 @@ barNP.addEventListener("click", function(){
       const conteudoAbreviado = conteudo.slice(0,50) + "...";
       const construindoCards = document.getElementById("sessao-cards");
       
-      
+      console.log(titulo);
       const novoCard = document.createElement("div");
-      novoCard.innerHTML = titulo + "<br>" + conteudoAbreviado ;
-      
+      novoCard.innerHTML = titulo + "<br>" + conteudoAbreviado;
+      novoCard.classList.add("card");
+
+      novoCard.dataset.titulos = titulo;
+      novoCard.dataset.conteudos = conteudo;
       construindoCards.append(novoCard);
+  
+      
   }
+
+  //Outra função
   botaoSair.addEventListener("click", sairDoPlublicar);
   function sairDoPlublicar(){
-      
+      telaAberta = false;
+      let conteudoECards = document.getElementById("conteudoCards").style.display="block";   
+       
+
+    input.style.display="none"; 
+    input2.style.display="none"; 
+    botoes.style.display="none"; 
   }
 
 
 });
+const cards = document.getElementById("sessao-cards");
+
+cards.addEventListener("click", conteudoDentroDoCard);
+function conteudoDentroDoCard(event){
+
+    if(event.target.classList.contains("card")){
+        const cardClicado = event.target;
+    
+        const tituloDentroDoCard = cardClicado.dataset.titulos;
+        const conteudoDentroDoCard = cardClicado.dataset.conteudos;
+
+    
+        const conteudoTexto = document.getElementById("coteudoPrincipal");
+        conteudoTexto.innerHTML = "<h1>" + tituloDentroDoCard + "</h1>" + "<p>" + conteudoDentroDoCard + "</p>";
+    console.log(tituloDentroDoCard);
+    } 
+}
+
+
+//Configuração de cards
 
 // classList.remove/add/touggle, setAttribute("atributo","valor")
